d518f30e795d4a5b46f94da1f2afb3914de7e1aa
João Mesquita <151409184+jfgmesquita@users.noreply.github.com>
2026-03-29 20:40:48 +0100

    feat: add Terraform remote backend configuration

---
+++ global/s3/main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "tf-state-2026"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
